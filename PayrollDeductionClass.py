bd021c362169d89899ce18be9cee36e6358866b1
campbell <campbell.com>
2022-09-27 13:01:18 -0500

    progress

---
+++ PayrollDeductionClass.py
@@ -0,0 +1,10 @@
+import random
+
+class Employee:
+    
+    def __init__(self,name,ID, department, job,salary):
+        self.__name = name
+        self.__ID = ID
+        self.__department = department
+        self.__job = job
+        self.__salary = salary 
